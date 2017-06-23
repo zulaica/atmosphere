020f49064a344f39e4142101aad16870be401728
David Zulaica <zulaica@gmail.com>
2017-06-22 17:07:59 -0700

    Scaffold microphone access

--- source/application.ts
+++ source/application.ts
@@ -1,1 +1,9 @@
 console.info('Hello, world!')
+
+navigator.mediaDevices.getUserMedia({ 'audio': true })
+  .then(() => {
+    console.info('Microphone access enabled.')
+  })
+  .catch(() => {
+    console.warn('Microphone access disabled.')
+  })
