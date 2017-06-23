d04ef66ce4e15bd28d5f3719f325ad3665f44995
David Zulaica <zulaica@gmail.com>
2017-06-22 18:47:30 -0700

    Move constraints into object literal

--- source/application.ts
+++ source/application.ts
@@ -1,9 +1,14 @@
+const constraints = {
+  audio: true,
+  video: false
+}
+
 const handleError = () =>
   console.warn('Microphone access disabled.')
 
 const handleSuccess = () =>
   console.info('Microphone access enabled.')
 
-navigator.mediaDevices.getUserMedia({ 'audio': true })
+navigator.mediaDevices.getUserMedia(constraints)
   .then(handleSuccess)
   .catch(handleError)
