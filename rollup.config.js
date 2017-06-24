8ae3a71cc99b84f2d79b1b1332a051cb2049f6c4
David Zulaica <zulaica@gmail.com>
2017-06-24 12:54:50 -0700

    Strip comments from dist js file with rollup cleanup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,11 @@
+import cleanup from 'rollup-plugin-cleanup'
+
 export default {
   entry: 'build/application.js',
   format: 'iife',
   dest: 'dist/application.js',
+  plugins: [ cleanup({
+    comments: ['ts3s']
+  }) ],
   sourceMap: true
 }
