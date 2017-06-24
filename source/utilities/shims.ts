0413358d15c497786b9f0829eacf948274754edf
David Zulaica <zulaica@gmail.com>
2017-06-24 14:08:55 -0700

    Clean up console output

--- source/utilities/shims.ts
+++ source/utilities/shims.ts
@@ -8,26 +8,13 @@
 /// <reference types="webrtc" />
 export function getUserMedia(constraints: MediaStreamConstraints) {
   if (!navigator.mediaDevices) {
-    console.log('🚫 No mediaDevices support.')
-
     navigator.mediaDevices = <MediaDevices>{}
   }
 
   if(!navigator.mediaDevices.getUserMedia) {
-    console.log('🚫 No getUserMedia support.')
-
     const getUserMedia = navigator.getUserMedia ||
                          navigator.mozGetUserMedia ||
                          navigator.webkitGetUserMedia
-
-    if(navigator.webkitGetUserMedia) {
-      console.log('Legacy Chrome/Opera')
-    }
-
-    if(navigator.mozGetUserMedia) {
-      console.log('Legacy Firefox')
-    }
-
     console.log(getUserMedia)
   }
 
