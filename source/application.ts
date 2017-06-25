4ac3a1f733aee8f3a662d8ba07fc618a075f172a
David Zulaica <zulaica@gmail.com>
2017-06-24 21:57:30 -0700

    Get and log current second in a 24 hour period

--- source/application.ts
+++ source/application.ts
@@ -16,6 +16,24 @@ const application = () => {
   getUserMedia(constraints)
     .then(handleSuccess)
     .catch(handleError)
+
+  setInterval(function() {
+    getCurrentSecond()
+      .then(logCurrentSecond)
+  }, 1000)
+}
+
+const getCurrentSecond = () => {
+  const currentTime = new Date()
+  return Promise.resolve(
+    (currentTime.getHours() * 3600) +
+    (currentTime.getMinutes() * 60) +
+    currentTime.getSeconds()
+  )
+}
+
+const logCurrentSecond = (second: number) => {
+  console.log(second)
 }
 
 window.addEventListener('load', application, false)
