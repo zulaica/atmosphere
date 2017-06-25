b06c43f1a2a5f3799a00d7efe67d46493457fadb
David Zulaica <zulaica@gmail.com>
2017-06-25 16:38:23 -0700

    Add linebreaks

--- source/utilities/helpers.ts
+++ source/utilities/helpers.ts
@@ -2,11 +2,13 @@ export const noop = () => {}
 
 export class Poller {
   intervalId: number
+
   start = (fn: Function, duration: number) => {
     if (!this.intervalId) {
       this.intervalId = setInterval(fn, duration)
     }
   }
+
   stop = () => {
     clearInterval(this.intervalId)
     this.intervalId = -1
