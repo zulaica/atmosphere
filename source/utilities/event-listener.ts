3e41a3b4eb8934b1e10eaf1c8146d551b737a915
David Zulaica <zulaica@gmail.com>
2017-07-14 00:16:47 -0700

    Refactor `Window.on` method by making better use of lib.d.ts

--- source/utilities/event-listener.ts
+++ source/utilities/event-listener.ts
@@ -1,13 +1,10 @@
-// tslint:disable-next-line
-declare global {
-  interface Window {
-    on(
-      event: string,
-      eventListener: EventListener,
-      options?: boolean | undefined
-    ): void
-  }
+interface Window {
+  on(
+    event: string,
+    eventListener: EventListener,
+    options?: boolean | undefined
+  ): void
 }
 
-export default Window.prototype.on = (event, eventListener, options = false) =>
+Window.prototype.on = (event, eventListener, options = false) =>
   addEventListener(event, eventListener, options)
