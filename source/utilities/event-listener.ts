1687ebffcd0c4c0fb2b7ca7a68c5cd9cf7d1794b
David Zulaica <zulaica@gmail.com>
2017-07-14 01:52:34 -0700

    DRY things up and add `onEventMethod` to Document

--- source/utilities/event-listener.ts
+++ source/utilities/event-listener.ts
@@ -1,10 +1,21 @@
-interface Window {
-  on(
+interface OnEventMethodInterface {
+  on: (
     event: string,
     eventListener: EventListenerOrEventListenerObject,
     options?: boolean | undefined
-  ): void
+  ) => void
 }
 
-Window.prototype.on = (event, eventListener, options = false) =>
-  addEventListener(event, eventListener, options)
+const onEventMethod =
+  (event: string,
+   eventListener: EventListenerOrEventListenerObject,
+   options = false) =>
+    addEventListener(event, eventListener, options)
+
+// tslint:disable-next-line
+interface Node extends OnEventMethodInterface { }
+// tslint:disable-next-line
+interface Window extends OnEventMethodInterface { }
+
+Document.prototype.on = onEventMethod
+Window.prototype.on = onEventMethod
