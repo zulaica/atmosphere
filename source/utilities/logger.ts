52680180f014e8415f056c681e63e8975b2797ce
David Zulaica <zulaica@gmail.com>
2017-12-17 11:44:16 -0800

    🔄 🛠  [#1] Add interface for and index signature to Console

--- source/utilities/logger.ts
+++ source/utilities/logger.ts
@@ -1,9 +1,13 @@
+interface ConsoleInterface {
+    // Not happy about the <any> Console index fix...
+    // tslint:disable-next-line:no-any
+    [index: string]: any
+}
+
 // TODO: method parameter should be limited to actual Console methods
 const log = (method: string, message: string | number) => {
   return Promise.resolve(
-    // Not happy about the <any> Console index fix...
-    // tslint:disable-next-line:no-any
-    (console as any)[method].call(console, message)
+    (console as ConsoleInterface)[method].call(console, message)
   )
 }
 
