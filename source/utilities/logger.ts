a3f20feb539d3c34bf8f375db8747a8259641aa0
David Zulaica <zulaica@gmail.com>
2017-12-17 17:06:24 -0800

    🔄 🛠  [#1] Remove comments

--- source/utilities/logger.ts
+++ source/utilities/logger.ts
@@ -1,10 +1,8 @@
 interface ConsoleInterface extends Console {
-    // Not happy about the <any> Console index fix...
     // tslint:disable-next-line:no-any
     [index: string]: any
 }
 
-// TODO: method parameter should be limited to actual Console methods
 const log = (method: string, message: string | number) => {
   return Promise.resolve(
     (console as ConsoleInterface)[method].call(console, message)
