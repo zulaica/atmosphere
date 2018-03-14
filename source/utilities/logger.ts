ec2ae581ec89f1799a1b165f3ea94d6ac84e1245
David Zulaica <zulaica@gmail.com>
2018-03-14 12:16:16 -0700

    🆕 🛠  WIP Style console messages

--- source/utilities/logger.ts
+++ source/utilities/logger.ts
@@ -3,8 +3,14 @@ interface ConsoleInterface extends Console {
       (value: string | boolean | undefined | Element) => {} | void
 }
 
+const CSS = {
+  default: 'font-family: monospace;',
+  log: 'color: #396039; background: #dfecdf;'
+}
+
 const log = (message: string | number, method: string = 'log') => {
   if (!(console as ConsoleInterface)[method]) {
+    // tslint:disable-next-line:no-console
     console.error(
       `'${method}' is not a valid Console method. Falling back to 'log'.`
     )
@@ -12,7 +18,8 @@ const log = (message: string | number, method: string = 'log') => {
   }
 
   return Promise.resolve(
-    (console as ConsoleInterface)[method].call(console, message)
+    (console as ConsoleInterface)[method]
+      .call(console, `%c${message}`, CSS.log.concat(' ', CSS.default))
   )
 }
 
